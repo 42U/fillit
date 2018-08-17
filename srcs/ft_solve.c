aea1fe573cb78714b693d294d0299ad3191a260a
Issac Smith <issmith@e1z3r7p25.42.us.org>
2018-08-17 08:49:57 -0700

    fixed most issues. still have bug with making smallest square

--- srcs/ft_solve.c
+++ srcs/ft_solve.c
@@ -6,7 +6,7 @@
 /*   By: issmith <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/03 05:15:27 by issmith           #+#    #+#             */
-/*   Updated: 2018/08/16 21:26:32 by issmith          ###   ########.fr       */
+/*   Updated: 2018/08/17 01:24:53 by issmith          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 int		ft_solve(int *g_order, int flag)
 {
 	g_char = 65 + flag;
-	if (g_pcs == 0)
+	if (!g_pcs)
 		return (1);
 	g_i = -1;
 	while (++g_i < g_base)
@@ -27,6 +27,8 @@ int		ft_solve(int *g_order, int flag)
 			if (ft_pc_fit(g_order[flag]))
 			{
 				ft_order_alph(g_order, flag);
+				ft_print_arr();
+				ft_putchar('\n');
 				g_pcs--;
 				if (ft_solve(g_order, flag + 1))
 					return (1);
